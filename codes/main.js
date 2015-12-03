d9fe1bf87ebcd344e6429aa20fd060bf5041702a
nocilol <nocilol@users.noreply.github.com>
2015-12-03 13:32:07 +0900

    Removed test codes in Main

--- codes/main.js
+++ codes/main.js
@@ -17,21 +17,3 @@ StageManager.init(1024, 768);
 // call block test
 ImageManager.init(function() {BlockTest.call()}, this);
 
-var someStr = "$glvar_#sofla_$fskg = 31 * #lsjf";
-function reduceStr(str) {
-	return str.replace(/[$#][$#\w]+/gi, function(varExp) {
-		console.log(varExp);
-		while (varExp.match(/[$#]/gi).length > 1) {
-			varExp = varExp.replace(/[$#]\w+/gi, function(subVarExp) {
-				if (subVarExp.charAt(subVarExp.length - 1) == '_')
-					return subVarExp
-				else
-					return '1';
-			});
-			console.log(varExp);
-		}
-		return varExp;
-	});
-};
-console.log(reduceStr(someStr));
-
